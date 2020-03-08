449fce94c6169e16bab27346655be1f63fdf8ed6
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2020-03-08 23:11:44 +0800

    formated code

--- src/main/java/com/khjxiaogu/EventDebuger/EventDebuger.java
+++ src/main/java/com/khjxiaogu/EventDebuger/EventDebuger.java
@@ -9,38 +9,39 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.reflections.Reflections;
 
-
-public class EventDebuger extends JavaPlugin{
+public class EventDebuger extends JavaPlugin {
 	static JavaPlugin plugin;
 
 	@Override
 	public void onDisable() {
 		super.onDisable();
 	}
+
 	@Override
 	public void onLoad() {
 		this.saveDefaultConfig();
 	}
+
 	@Override
 	public void onEnable() {
-		plugin=this;
-		//Bukkit.getPluginManager().registerEvent(event, listener,EventPriority.MONITOR,new Logger(), plugin);
-		for(String pack:getConfig().getStringList("packages")) {
-		Reflections reflections = new Reflections(pack);
-			Set<Class<? extends Event>> events = 
-				    reflections.getSubTypesOf(Event.class);
-			for(Class<? extends Event> ev:events) {
-				Logger log=new Logger();
+		plugin = this;
+		// Bukkit.getPluginManager().registerEvent(event,
+		// listener,EventPriority.MONITOR,new Logger(), plugin);
+		for (String pack : getConfig().getStringList("packages")) {
+			Reflections reflections = new Reflections(pack);
+			Set<Class<? extends Event>> events = reflections.getSubTypesOf(Event.class);
+			for (Class<? extends Event> ev : events) {
+				Logger log = new Logger();
 				try {
-					Bukkit.getPluginManager().registerEvent(ev,new Listener() {},EventPriority.MONITOR,log, plugin);
-				}catch(Throwable t) {
-					getLogger().warning("Event "+ev.getName()+"register failed!");
+					Bukkit.getPluginManager().registerEvent(ev, new Listener() {
+					}, EventPriority.MONITOR, log, plugin);
+				} catch (Throwable t) {
+					getLogger().warning("Event " + ev.getName() + "register failed!");
 				}
-				
+
 			}
 		}
 		super.onEnable();
 	}
-
 
 }
--- src/main/java/com/khjxiaogu/EventDebuger/Logger.java
+++ src/main/java/com/khjxiaogu/EventDebuger/Logger.java
@@ -8,34 +8,34 @@ import org.bukkit.event.EventException;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.EventExecutor;
 
-public class Logger implements EventExecutor,Listener {
+public class Logger implements EventExecutor, Listener {
 
 	public Logger() {
 	}
 
 	public void execute(Listener listener, Event event) throws EventException {
-		if(event!=null) {
-			StringBuilder sb=new StringBuilder(event.getEventName());
+		if (event != null) {
+			StringBuilder sb = new StringBuilder(event.getEventName());
 			sb.append("[");
-			boolean nfirst=false;
-			Class<?> clazz=event.getClass();
-	        while (clazz != Object.class) { 
-				for(Field f:clazz.getDeclaredFields()) {
+			boolean nfirst = false;
+			Class<?> clazz = event.getClass();
+			while (clazz != Object.class) {
+				for (Field f : clazz.getDeclaredFields()) {
 					f.setAccessible(true);
-					if(!Modifier.isStatic(f.getModifiers())) {
+					if (!Modifier.isStatic(f.getModifiers())) {
 						try {
-							if(nfirst) {
+							if (nfirst) {
 								sb.append(",");
-							}else
-								nfirst=true;
-							sb.append((String.format("%s=%s",f.getName(),f.get(event).toString())));
+							} else
+								nfirst = true;
+							sb.append((String.format("%s=%s", f.getName(), f.get(event).toString())));
 						} catch (IllegalArgumentException e) {
 						} catch (IllegalAccessException e) {
 						}
 					}
 				}
 				clazz = clazz.getSuperclass();
-	        }
+			}
 			sb.append("]");
 			EventDebuger.plugin.getLogger().info(sb.toString());
 		}
